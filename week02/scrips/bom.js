9073f23b53008fca8657ab523cd3c88dd1f915fa
joekabusko-dot <jkabusakoiviti@byupathway.edu>
2026-09-09 00:48:15 +0200

    improve

--- week02/scrips/bom.js
+++ week02/scrips/bom.js
@@ -1,3 +1,8 @@
+// Create an array of the books of the Book of Mormon
+const bookofMormon = ['1nephi', '2nephi', 'jacob', 'enos', 'jarom', 'omni', 'wmosiah', 'mosiah', 'alma', 'helaman', '3nephi', '4nephi', 'mormon', 'ether', 'moroni'];
+
+
+
 // select elements from the DOM
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
@@ -7,9 +12,35 @@ const list = document.querySelector('#list');
 button.addEventListener('click', function () {
     //check if the user has entered a value
     if (input.value.trim() !== '') {
+
+        const cleanedValue = input.value.trim();
+
+        // 1. check if the book is valid
+        const isBookValid = bookofMormon.some(book =>
+            cleanedValue.toLowerCase().replace(/ /g, '').startsWith(book)
+        );
+        if (!isBookValid) {
+            alert("This book is not part of the Book of Mormon.");
+            return;
+        }
+
+        // 2. Limit the number of items in the list to 10
+        if (list.children.length >= 10) {
+            alert("Limit reached : maximum 10 elements.");
+            return;
+        }
+
+        // 3. avoid duplicates in the list
+        const currentItems = Array.from(list.querySelectorAll('li')).map(item =>
+            item.textContent.replace('❌', '').trim()
+        );
+        if (currentItems.includes(cleanedValue)) {
+            alert("This chapter is already in the list.");
+            return;
+        }
         //create list item and give it the value of the input 
         const li = document.createElement('li');
-        li.textContent = input.value;
+        li.textContent = cleanedValue;
         //create a button and add  a click event listener  
         const deleteButton = document.createElement('button');
         deleteButton.textContent = '❌';
